a1341666a71079fc7d8a23957b5c37bb333fc2f0
crittersik <ola.bej@gmail.com>
2015-06-02 09:09:18 +0100

    Some minimal changes

--- plot2.R
+++ plot2.R
@@ -1,4 +1,5 @@
-# read text file - I read only part of the data that contains data relevant for assignment 
+# First step - read text file 
+# I read only part of the data that contains data relevant for assignment 
 # (helping my memory to process the data)
 
 mydata <- read.table("household_power_consumption.txt", sep=";", header=TRUE, skip=66000,nrows =50000, col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
